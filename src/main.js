413f246e1314a1346de6dc2cd85196e36532bf97
zhangzw <cao_fg@163.com>
2018-08-16 18:46:25 +0800

    一丢丢

--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import { Button, Select } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import { Button,
+  Select,
+  Form,
+  FormItem,
+  Input,
+  InputNumber
+} from 'element-ui'
+
 Vue.use(Button)
 Vue.use(Select)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(InputNumber)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    login
+  <div class="login">
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="名称">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      labelPosition: 'right',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.login {
+  width: 500px;
+  height: 300px;
+  position: relative;
+}
 </style>
